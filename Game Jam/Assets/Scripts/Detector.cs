26952ce30619e05e9808937d9e3bb8caa1b3ae4d
Subhash <136467430+Subhashraj484@users.noreply.github.com>
2024-03-14 21:45:37 +0530

    Detector
    rought detector

--- Game Jam/Assets/Scripts/Detector.cs
+++ Game Jam/Assets/Scripts/Detector.cs
@@ -1,43 +1,62 @@
-using Unity;
-using System.Collections;
+
 using System.Collections.Generic;
 using UnityEngine;
-using Unity.Mathematics;
-using System;
-using UnityEditor.ShaderGraph.Internal;
 
 public class Detector : MonoBehaviour
 {
-    [SerializeField] Transform player;
+
+    [SerializeField] List<Transform> players = new();
     [SerializeField] LayerMask detectionLayer;
+    [SerializeField] Transform[] points = new Transform[2];
+    [SerializeField] float changeDistance = 2;
+    [SerializeField] float moveSpeed = 10;
     bool detected;
     Vector3 direction;
     float angle ;
-    float lowLeftAngle = 3.5f;
-    float lowRightAngle = -3.5f;
-    float topLeftAngle = 21f;
-    float topRightAngle = 8f;
+    float lowAngle = 45;
+    float maxAngle = 125f;
     bool inSight;
+    Color rayColor = Color.green;
+    int currentIndex =0;
+    
 
     void Update()
     {
-        direction = player.position - transform.position;
-        direction.Normalize();
+        if(transform.position ==   Vector3.zero)
+        {
+            transform.position = new Vector3(0.1f,0,0);
+        }
+        
+        Vector3 moveDirection = points[currentIndex].position;
+        moveDirection.Normalize();
+        if(Vector3.Distance(transform.position , points[currentIndex].position) < changeDistance)
+        {
+            currentIndex++;
+            if(currentIndex > 1) currentIndex = 0;
+        }
+        transform.position += moveDirection*moveSpeed*Time.deltaTime;
+
+        foreach(Transform player in players)
+        {
+            direction = player.position - transform.position;
+            direction.Normalize();
 
         // angle = MathF.Tan(direction.x/direction.y);
         angle = Vector2.Angle(player.position , transform.position);
 
-        if(angle <=lowLeftAngle && angle > lowRightAngle)
+        if(angle <= maxAngle && angle > lowAngle)
         {
             inSight = true;
+            rayColor = Color.red;
         }
         else
         {
             inSight = false;
+            rayColor = Color.green;
         }
 
         RaycastHit2D hitinfo = Physics2D.Raycast(transform.position , direction ,100f   ,detectionLayer);
-        if(hitinfo.collider.CompareTag("Player"))
+        if(hitinfo.collider != null && hitinfo.collider.CompareTag("Player") && inSight )
         {
             
             detected = true;
@@ -46,10 +65,16 @@ public class Detector : MonoBehaviour
         {
             detected = false;
         }
+        
+
+        Debug.DrawRay(transform.position , direction*100f , rayColor);
+        }
+        
+        
     }
 
-    void OnDrawGizmos()
-    {
-        Gizmos.DrawRay(transform.position , direction*100f);
-    }
+    // void OnDrawGizmos()
+    // {
+    //     Gizmos.Draw(transform.position , direction*100f , rayColor);
+    // }
 }
